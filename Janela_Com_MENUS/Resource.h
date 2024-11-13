c5b9b7db257f8b525a74056e85a93de0ac860bb3
Blako <gustavo.reis@a.ucb.br>
2024-11-13 12:35:21 -0300

    gfdf
    hgfdfgn

---
+++ Janela_Com_MENUS/Resource.h
@@ -0,0 +1,10 @@
+#define IDR_MYMENU 101
+#define IDI_MYICON 201
+#define IDD_ABOUT 9004
+#define IDC_STATIC 303
+
+#define ID_FILE_EXIT 9001
+#define ID_STUFF_GO 9002
+#define ID_STUFF_GO_SOMEWERE 9003
+#define IDOK 1
+#define IDCANCEL 2
